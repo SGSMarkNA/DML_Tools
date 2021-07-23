737809207bc4aca200712ab431f632baf04775cf
Drew Loveridge <mayaenite@hotmail.com>
2021-07-23 04:51:04 -0400

    Add method to assign nodes to the shader engine

--- DML_Maya/Maya_Nodes/Standered_Nodes/Shading_Engine.py
+++ DML_Maya/Maya_Nodes/Standered_Nodes/Shading_Engine.py
@@ -11,11 +11,15 @@ class Shading_Engine(Selection_Set):
 	def createNode(self,*args,**kwargs):
 		""""""
 		name = kwargs.pop("name",None)
-		
-		kwargs = dict(renderable=True,empty=True)
+		kwargs = dict(renderable=True,noSurfaceShader=True,empty=True)
 		if name is not None:
 			kwargs['name']=name
 		return cmds.sets(**kwargs)
+	#----------------------------------------------------------------------
+	def include(self,*items):
+		#self.__rshift__(items)
+		if len(items):
+			cmds.sets(*items,forceElement=self)
 	##----------------------------------------------------------------------
 	def Assine_To_Material(self,shader):
 		shader_plug = str(shader)+".outColor"
