29a6cbecc82ea6b537e3b4e6f3ced9b53c6d026e
Drew Loveridge <mayaenite@hotmail.com>
2023-02-28 16:33:38 -0500

    not sure why i had to changed this and right now don't care it works

--- DML_PYQT/QT/User_Data_Generators.py
+++ DML_PYQT/QT/User_Data_Generators.py
@@ -3,9 +3,9 @@ from . import Qt,QStandardItem
 __all__ = ["userRole_generator","userType_generator"]
 ########################################################################
 class Counter(itertools.count):
-	#----------------------------------------------------------------------
-	def __init__(self,start=1,step=1):
-		super(Counter,self).__init__(start,step)
+	##----------------------------------------------------------------------
+	#def __init__(self,start=1,step=1):
+		#super(Counter,self).__init__(start=start,step=step)
 	#----------------------------------------------------------------------
 	def __call__(self):
 		return next(self)
